da48183d9815123ab07192217d5156dd17bfc410
AliHakim773 <alihakim7732@gmail.com>
2023-12-07 10:03:23 +0200

    cennected the database

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$db_user = 'root';
+$db_passwod = null;
+$db_name = 'e-com-db';
+
+$mysqli = new mysqli($host, $db_user, $db_passwod, $db_name);
+
+if ($mysqli->connect_error) {
+    die("" . $mysqli->connect_error);
+    exit();
+}
