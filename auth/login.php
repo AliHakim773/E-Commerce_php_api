ad5a7f2c6d7d20ae8d53854ce91af28bb7ac63c5
AliHakim773 <alihakim7732@gmail.com>
2023-12-07 10:52:43 +0200

    added login with jwt token

--- auth/login.php
+++ auth/login.php
@@ -1,1 +1,51 @@
 <?php
+
+include('../vendor/autoload.php');
+include('../connection.php');
+
+use Firebase\JWT\JWT;
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$response = [];
+try {
+    $query = $mysqli->prepare('select user_id, username, password, user_type from users where username=?');
+    $query->bind_param('s', $username);
+    $query->execute();
+
+    $query->store_result();
+    $num_rows = $query->num_rows();
+
+    if ($num_rows == 0) {
+        $response['status'] = 'false';
+        $response['msg'] = 'user not found';
+
+        echo json_encode($response);
+        exit();
+    }
+
+    $query->bind_result($id, $name, $hashed_password, $user_type);
+    $query->fetch();
+
+    if (password_verify($password, $hashed_password)) {
+        $key = 'ez4me';
+        $payload = [
+            'user_id' => $id,
+            'username' => $name,
+            'user_type' => $user_type,
+            'exp' => time() + 3600
+        ];
+        $jwt = JWT::encode($payload, $key, 'HS256');
+
+        $response['status'] = 'true';
+        $response['msg'] = 'loged in';
+        $response['jwt'] = $jwt;
+        echo json_encode($response);
+    }
+} catch (Exception $e) {
+    $response["status"] = "false";
+    $response["msg"] = $e->getMessage();
+
+    echo json_encode($response);
+}
