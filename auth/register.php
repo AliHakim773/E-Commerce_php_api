7fe4a2bd53325deaed62d2b75e6cf4d0d1d1a9a0
AliHakim773 <alihakim7732@gmail.com>
2023-12-07 10:33:20 +0200

    added check for the username if it exist

--- auth/register.php
+++ auth/register.php
@@ -11,6 +11,21 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 $response = [];
 try {
+    $query = $mysqli->prepare('select username from users where username=?');
+    $query->bind_param('s', $username);
+    $query->execute();
+
+    $query->store_result();
+    $num_rows = $query->num_rows();
+
+    if ($num_rows != 0) {
+        $response["status"] = "false";
+        $response["msg"] = "username already exist";
+
+        echo json_encode($response);
+        exit();
+    }
+
     $query = $mysqli->prepare('insert into users(username, password, user_type) 
         values(?,?,?)');
     $query->bind_param('ssi', $username, $hashed_password, $user_type);
